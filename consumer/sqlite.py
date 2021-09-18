ef9c52d87be4f03c6961bbd2ec9b516e368e83f2
GwenealB <gwenealbenita@gmail.com>
2021-09-19 05:29:42 +0800

    Added new data to SQLite DB

--- consumer/sqlite.py
+++ consumer/sqlite.py
@@ -1,7 +1,13 @@
 import pandas, sqlite3
 
+__folder = ''
+AB_NYC_2019 = __folder + 'AB_NYC_2019'
+AB_NYC_2020 = __folder + 'AB_NYC_2020'
+AB_NYC_2021 = __folder + 'AB_NYC_2021'
 
 
 def consume(table_name:str):
   conn = sqlite3.connect('database/sqlite.db')
-  return pandas.read_sql_query(f'SELECT * FROM {table_name}', conn)
+  data = pandas.read_sql_query(f'SELECT * FROM {table_name}', conn)
+  conn.close()
+  return data
