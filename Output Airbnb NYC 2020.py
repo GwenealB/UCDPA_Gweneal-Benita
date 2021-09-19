cc4eaad1b4b05c9722c7d28fecbb0f3bd908a074
GwenealB <gwenealbenita@gmail.com>
2021-09-20 00:48:09 +0800

    Updated Project report

--- Output Airbnb NYC 2020.py
+++ Output Airbnb NYC 2020.py
@@ -1,14 +1,14 @@
 from airbnb_analysis.consumer import CSV
 from airbnb_analysis import consumer
-data = CSV.consume('data/AB_NYC_2019.csv')
+data = CSV.consume('data/AB_NYC_2020.csv')
 from airbnb_analysis.analysis import AirbnbAnalyzer
-AB2019 = AirbnbAnalyzer(data)
+AB2020 = AirbnbAnalyzer(data)
 
 
 
-AB2019.room_type_dist_figure('output/AB_NYC_2019_RoomTypePie.png')
-AB2019.price_intensity_map('output/AB_NYC_2019_PriceIntensityMap.png')
-AB2019.most_reviewed_per_location('output/AB_NYC_2019_MostReviewedPerLocationBar.png')
-AB2019.rent_dist_by_neighborhood('output/AB_NYC_2019_RentDistributionByNeighborhoodPie.png')
-AB2019.price_bar_per_location('output/AB_NYC_2019_PriceBarPerLocation.png')
-AB2019.price_dist_histogram('output/AB_NYC_2019_PriceHistogram.png')
+AB2020.room_type_dist_figure('output/AB_NYC_2020_RoomTypePie.png')
+AB2020.price_intensity_map('output/AB_NYC_2020_PriceIntensityMap.png')
+AB2020.most_reviewed_per_location('output/AB_NYC_2020_MostReviewedPerLocationBar.png')
+AB2020.rent_dist_by_neighborhood('output/AB_NYC_2020_RentDistributionByNeighborhoodPie.png')
+AB2020.price_bar_per_location('output/AB_NYC_2020_PriceBarPerLocation.png')
+AB2020.price_dist_histogram('output/AB_NYC_2020_PriceHistogram.png')
